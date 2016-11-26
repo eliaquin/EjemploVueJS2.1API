d4de12a5e65566ec83353ba4943fb5d6cd2c3df5
Eliaquin Encarnacion <eliaquin@gmail.com>
2016-11-25 20:39:19 -0400

    Refinamientos y mejoras

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ var app = new Vue({
         registros: []
     },
     methods: {
-        filtroRNC: function(parametro){
-            return this.registros.filter(registro=>registro.RNC.includes(parametro));
-        },
-        filtroNombre: function(parametro){
-            return this.registros.filter(registro=>registro.Nombre.includes(parametro));
+        /*filtroRNC: function (parametro) {
+            return this.registros.filter(function (el) { return el.RNC.includes(parametro) });
+        },*/
+        filtroNombre: function (parametro) {
+            return this.registros.filter(function (el) { return el.Nombre.toUpperCase().includes(parametro.toUpperCase()) });
         },
         buscar: function () {
 
-            /*  var url = 'http://app.mopc.gob.do/Utilitario/api/rnc/buscarporcriterio?criterio=' + this.criterio + '&uid=7c9e6679-7425-40de-944b-e07fc1f90ae7';
+            var url = 'http://app.mopc.gob.do/Utilitario/api/rnc/buscarporcriterio?criterio=' + this.criterio + '&uid=7c9e6679-7425-40de-944b-e07fc1f90ae7';
   
               axios.get(url)
                   .then(function (response) {
                       app.registros = JSON.parse(response.data.ResponseText);
-                  })*/
+                  })
 
-
+            /*
             var url = 'http://app.mopc.gob.do/Utilitario/api/rnc/buscarporcriterio?criterio=' + this.criterio + '&uid=7c9e6679-7425-40de-944b-e07fc1f90ae7';
             var xhr = new XMLHttpRequest();
             xhr.open("GET", encodeURI(url), true);
@@ -40,7 +40,7 @@ var app = new Vue({
                 if (resultado.Success) {
                     app.registros = JSON.parse(resultado.ResponseText);
                 }
-            }
+            }*/
         }
     }
 });
