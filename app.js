fe4a8bd4c1e83256cdec4cec435c6785350a6590
Eliaquin Encarnacion <eliaquin@gmail.com>
2016-11-27 21:21:18 -0400

    Agregada instancia para acceder a objeto desde un callback function

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-Vue.filter('porRNC', function (value) {
-    return value.RNC === '01300397963';
-
-});
-
-var app = new Vue({
+new Vue({
     el: '#busqueda',
     data: {
         criterio: '',
@@ -18,29 +13,12 @@ var app = new Vue({
             return this.registros.filter(function (el) { return el.Nombre.toUpperCase().includes(parametro.toUpperCase()) });
         },
         buscar: function () {
-
+            var vm = this;
             var url = 'http://app.mopc.gob.do/Utilitario/api/rnc/buscarporcriterio?criterio=' + this.criterio + '&uid=7c9e6679-7425-40de-944b-e07fc1f90ae7';
-  
-              axios.get(url)
-                  .then(function (response) {
-                      app.registros = JSON.parse(response.data.ResponseText);
-                  })
-
-            /*
-            var url = 'http://app.mopc.gob.do/Utilitario/api/rnc/buscarporcriterio?criterio=' + this.criterio + '&uid=7c9e6679-7425-40de-944b-e07fc1f90ae7';
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", encodeURI(url), true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.send();
-
-            xhr.onload = function () {
-                var resultado = JSON.parse(xhr.responseText);
-
-                if (resultado.Success) {
-                    app.registros = JSON.parse(resultado.ResponseText);
-                }
-            }*/
+            axios.get(url)
+                .then(function (response) {
+                    vm.registros = JSON.parse(response.data.ResponseText);
+                });
         }
     }
 });
